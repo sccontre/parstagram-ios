9a1ba4f12f5b494ae88c76294432d1061a2a1159
Simonne Contreras <sccontre@ucsd.edu>
2021-05-09 18:04:22 -0700

    Initial project creation

--- parstagram/AppDelegate.swift
+++ parstagram/AppDelegate.swift
@@ -6,18 +6,20 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "zCPn9T8vLANK2n2vWbSgq9AYCMumJCOGGLsbyOcH"
+                $0.clientKey = "KSmGL9Er5gHfj33AFzrvcNT06ZVnZXhOTNArSPrS"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
